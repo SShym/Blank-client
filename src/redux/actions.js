a62c7403e808d2d72883989b419e1d55d4285996
SShym <serh.shym@gmail.com>
2022-12-04 14:36:29 +0200

    ys

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -15,7 +15,7 @@ export const SET_AUTHDATA = 'SET_AUTHDATA';
 export const LOGOUT = 'LOGOUT';
 
 const API = axios.create({ 
-    baseURL: 'https://sqmr.onrender.com/' 
+    baseURL: 'https://sqmr.onrender.com/123' 
     // baseURL: 'http://localhost:5000/' 
 });
 
